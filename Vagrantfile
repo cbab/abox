305b5ec48b12282e3dd5c91c0ee2d333623fe70b
Christian Babeux <christian.babeux@efficios.com>
2014-07-02 16:57:43 -0400

    Vagrant setup to deploy abox
    Signed-off-by: Christian Babeux <christian.babeux@efficios.com>

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+VAGRANTFILE_API_VERSION = "2"
+
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "trusty"
+  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+  config.vm.hostname = "abox-0"
+  config.vm.provision "shell" do |s|
+    s.path = "scripts/deploy.sh"
+    s.args = ENV['TRACEVISOR_IP']
+  end
+  config.vm.provision "shell", path: "scripts/deploy-lttng-source.sh"
+  config.vm.network "private_network", type: "dhcp"
+end
---
+++ scripts/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if test $# -lt 1; then
+	echo "Usage : $0 <tracevisor hostname/ip>"
+	exit 1
+fi
+
+TRACEVISORIP=$1
+TRACEVISORPORT=5000
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+#apt-get -y install git libavahi-client3 avahi-daemon libavahi-client-dev libavahi-common-dev make gcc curl
+apt-get -y install mongodb python3 git
+
+# Get analysis scripts
+cd /usr/local/src
+git clone https://github.com/jdesfossez/lttng-analyses.git
+
+# Get tracevisor SSH key
+mkdir -p ~/.ssh
+curl -s http://${TRACEVISORIP}:${TRACEVISORPORT}/trace/api/v1.0/ssh|grep tracevisor | cut -d '"' -f2 | cut -d "\\" -f1 >> ~/.ssh/authorized_keys2
+
+
